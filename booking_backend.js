a2fa22de4b53d918fc0cb1550986f632d0048370
Sajid Haque <sajidul.h.sajid@gmail.com>
2026-06-22 08:22:20 +1000

    Change server listen address to '0.0.0.0'

--- booking_backend.js
+++ booking_backend.js
@@ -66,4 +66,4 @@ app.patch('/api/bookings/:id', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`✅ Server running on port ${PORT}`));
